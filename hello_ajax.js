6c862c9def91965a41863d9a84a1d3adc1919c76
DriftwoodJP <DriftwoodJP@users.noreply.github.com>
2016-12-20 15:41:41 +0900

    Add: GET method Pattern

---
+++ hello_ajax.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('btn').addEventListener('click', function() {
+    var result = document.getElementById('result');
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          result.textContent = xhr.responseText;
+        } else {
+          result.textContent = 'サーバエラーが発生しました。';
+        }
+      } else {
+        result.textContent = '通信中...';
+      }
+    };
+    xhr.open('GET', 'hello_ajax.php?name=' +
+      encodeURIComponent(document.getElementById('name').value), true);
+    xhr.send(null);
+  }, false);
+}, false);
